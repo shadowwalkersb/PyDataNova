6f8f598056a1e47cb7f0137b1b3dc929bc2bbbdf
shadow_walker <shadowwalkersb@gmail.com>
2025-08-20 23:55:55 -0500

    core

---
+++ app/api/core/config.py
@@ -0,0 +1,4 @@
+# Database URIs / credentials
+ELEPHANT_URI = "postgresql://user:pass@host:port/dbname"
+SUPRA_URI = "postgresql://user:pass@host:port/dbname"
+MONGO_URI = "mongodb+srv://user:pass@cluster.mongodb.net/dbname?retryWrites=true&w=majority"
