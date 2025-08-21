802184d932fa55cd90458df2b7a0ca56dfc6805a
shadow_walker <shadowwalkersb@gmail.com>
2025-08-20 23:55:55 -0500

    STASH

--- app/api/fastapi/main.py
+++ app/api/fastapi/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from api.fastapi.routers import users, items
 
 app = FastAPI(title="PyDataNova FastAPI v6")
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to PyDataNova FastAPI v6"}
+# Include routers
+app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(items.router, prefix="/items", tags=["items"])
