c5d5001a3fc0bc3721b69efddffde7e11ce1f14d
shadow_walker <shadowwalkersb@gmail.com>
2025-08-21 11:31:29 -0500

    flask CORS

--- app/api/flask/app.py
+++ app/api/flask/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.users import bp as users_bp
 from routes.items import bp as items_bp
 
 app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:5500",
+                   "http://127.0.0.1:5500",
+                   ])  # allow your frontend
 
 app.register_blueprint(users_bp, url_prefix="/users")
 app.register_blueprint(items_bp, url_prefix="/items")
