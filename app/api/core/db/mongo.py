3333cb7e149afb85ca69046eac3e3abd0777180d
shadow_walker <shadowwalkersb@gmail.com>
2025-08-20 23:55:55 -0500

    core

--- app/api/core/db/mongo.py
+++ app/api/core/db/mongo.py
@@ -1,11 +1,6 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB = os.getenv("MONGO_DB", "pydatanova")
-
-client = MongoClient(MONGO_URI)
-db = client[MONGO_DB]
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+client = MongoClient(MONGO_URL)
+mongo_db = client["pydatanova"]
---
+++ app/api/core/models/mongo.py
@@ -0,0 +1,5 @@
+# MongoDB collections
+from ..db.mongo import mongo_db
+
+users_collection = mongo_db["users"]
+items_collection = mongo_db["items"]
