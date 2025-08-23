a0e37a62824193eb0515cbb5fb52f7704da1477d
shadow_walker <shadowwalkersb@gmail.com>
2025-08-23 13:42:32 -0500

    pipeline front

---
+++ app/versions/v7/js/pipeline.js
@@ -0,0 +1,7 @@
+import { FASTAPI_URL } from "../../../js/config.js";
+
+const runBtn = document.getElementById("runPipelineBtn");
+
+runBtn.addEventListener("click", async () => {
+    alert("Pipeline execution is not implemented in this version.");
+});
