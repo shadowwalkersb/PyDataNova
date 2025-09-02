b7b771e0772c2bc284cf45bdaa2dc7753ac022c9
shadow_walker <shadowwalkersb@gmail.com>
2025-09-02 01:40:28 -0500

    front

--- app/api/flask_service/app.py
+++ app/api/flask_service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_service.routes.users_mock import bp as users_mock_bp
 from flask_service.routes.items_mock import bp as items_mock_bp
@@ -21,3 +21,14 @@ app.register_blueprint(items_bp, url_prefix="/items")
 @app.route("/")
 def index():
     return {"message": "PyDataNova Flask Service running..."}
+
+@app.route("/image/face-detect")
+def face_detect():
+    return jsonify({"faces_detected": 2})
+
+@app.route("/image/enhance")
+def enhance():
+    return jsonify({"status": "success", "message": "image enhanced"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
